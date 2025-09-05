d155b7d10261b02b8132ab74ed8dfb4c458c1ebd
Anchal Chaurasia <anchalchaurasia2005@gmail.com>
2025-09-06 00:35:58 +0530

    Update LatestInterviewsList.jsx

--- app/(main)/dashboard/_components/LatestInterviewsList.jsx
+++ app/(main)/dashboard/_components/LatestInterviewsList.jsx
@@ -25,9 +25,13 @@ function InterviewCard({ interview,viewDetail=false}) {
         
         </div>
         <h2 className='mt-3 font-bold text-lg'>{interview?.jobPosition}</h2>
-        <h2 className='mt-2 flex justify-between text-gray-500'>{interview?.duration}
-            <span className='text-green-700'>{interview['interview-feedback']?.length} Candidates</span>
-        </h2>
+        <h2 className='mt-2 flex justify-between text-gray-500'>
+  {interview?.duration}
+  <span className='text-green-700'>
+    {(interview?.['interview-feedback']?.length ?? 0)} Candidates
+  </span>
+</h2>
+
         {!viewDetail? <div className='flex gap-3 w-full mt-5'>
             <Button variant='outline' className={'w-small'} onClick={copyLink}> <Copy /> Copy Link</Button>
             <Button className={'w-small'} onClick={onSend}> <Send /> </Button>
